40eddd0a649151d07d18ee9067884797f62b8625
Ironman Stark <ironman@professionalcipher.com>
2022-03-17 21:37:09 +0530

    map_working

--- app/src/main/java/com/example/mapview_test/MainActivity.java
+++ app/src/main/java/com/example/mapview_test/MainActivity.java
@@ -1,26 +1,37 @@
 package com.example.mapview_test;
 
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.mapbox.maps.MapView;
-import com.mapbox.maps.Style;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 
 
-public class MainActivity extends AppCompatActivity   {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private MapView mapView;
-//    private MapboxMap mapboxMap;
+    private MapboxMap mapboxMap;
+    LocationEngine locationEngine;
+    LocationLayerPlugin locationLayerPlugin;
+    PermissionsManager permissionsManager;
+    Location originLayout;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
-//        Mapbox.getInstance(this,getString(R.string.access_token));
+        Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_main);
         mapView = findViewById(R.id.mapView);
-
-        //        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
-
+        mapView.getMapAsync(this);
+        mapView.onCreate(savedInstanceState);
     }
 
     @Override
@@ -35,17 +46,17 @@ public class MainActivity extends AppCompatActivity   {
         mapView.onStop();
     }
 
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        mapView.onPause();
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        mapView.onResume();
-//    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
 
     @Override
     protected void onDestroy() {
@@ -59,6 +70,20 @@ public class MainActivity extends AppCompatActivity   {
         mapView.onLowMemory();
     }
 
+    @Override
+    protected void onSaveInstanceState( @NonNull Bundle outState ) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
 
+    @Override
+    public void onMapReady( @NonNull MapboxMap mapboxMap ) {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(Style.DARK);
 
+        mapboxMap.getUiSettings().setZoomGesturesEnabled(true);
+        mapboxMap.getUiSettings().setScrollGesturesEnabled(true);
+        mapboxMap.getUiSettings().setAllGesturesEnabled(true);
+        System.out.println("Hello Map IS Ready");
+    }
 }
